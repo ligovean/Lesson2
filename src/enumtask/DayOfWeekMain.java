f5953ac6adef9ebe5dc0a0655adf0e542b053894
ligovean <ligomail@rambler.ru>
2019-05-02 17:38:41 +0300

    HW

--- src/enumtask/DayOfWeekMain.java
+++ src/enumtask/DayOfWeekMain.java
@@ -1,6 +1,24 @@
 package enumtask;
 
 public class DayOfWeekMain {
+
     public static void main(final String[] args) {
+
+        System.out.println(getWorkingHours(DayOfWeek.MONDAY)); //Пример из задания
+        System.out.println();
+        System.out.println("=============/Вывод всех дней недели/=============");
+        for (DayOfWeek d: DayOfWeek.values()){
+            System.out.printf("Сейчас %s. %s \n",d.getName(),getWorkingHours(d));
+        }
+    }
+
+    public static String getWorkingHours(DayOfWeek d){
+        //В ходе обсуждения с заказчиком выяснилось,
+        // что он хочет текстовый вывод в формате
+        // "Осталось n ч. до конца рабочей недели!"
+
+        if (d.getHoursQty()==0)
+            return "Выходной день!";
+        return "Осталось " + Integer.toString(d.getHoursQty()) + " ч. до конца рабочей недели!";
     }
 }
