6a56d228b715ffaabab2554105a1525496c77803
ligovean <ligomail@rambler.ru>
2019-05-02 22:03:57 +0300

    Определение рабочих часов для каждого дня

--- src/enumtask/DayOfWeekMain.java
+++ src/enumtask/DayOfWeekMain.java
@@ -6,19 +6,19 @@ public class DayOfWeekMain {
 
         System.out.println(getWorkingHours(DayOfWeek.MONDAY)); //Пример из задания
         System.out.println();
+
+        //Для вывода по всем дням недели
         System.out.println("=============/Вывод всех дней недели/=============");
+
+        System.out.printf("В этой неделе %s рабочих часов.\n", DayOfWeek.MONDAY.getWeekHours());
         for (DayOfWeek d: DayOfWeek.values()){
-            System.out.printf("Сейчас %s. %s \n",d.getName(),getWorkingHours(d));
+            System.out.printf("Сейчас %s. %s\n",d.getName(),getWorkingHours(d));
         }
     }
 
     public static String getWorkingHours(DayOfWeek d){
-        //В ходе обсуждения с заказчиком выяснилось,
-        // что он хочет текстовый вывод в формате
-        // "Осталось n ч. до конца рабочей недели!"
-
         if (d.getHoursQty()==0)
             return "Выходной день!";
-        return "Осталось " + d.getHoursQty() + " ч. до конца рабочей недели!";
+        return "Осталось " + d.getRemainHours() + " ч. до конца рабочей недели!";
     }
 }
