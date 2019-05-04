a764d0df0133cccc49ba64d9d6107db36ea5e63d
ligovean <ligomail@rambler.ru>
2019-05-04 17:11:43 +0300

    Enum and Exceptions

--- src/exceptiontask/Main.java
+++ src/exceptiontask/Main.java
@@ -4,15 +4,20 @@ public class Main {
     public static void main(String[] args) {
         String[][] arr1 = {{"1","2","3"},{"01","02","03"},{"01","02","03"}};
 
+
         try {
-            myArrayExchange(arr1,2);
+            myArrayExchange(arr1,3);
         } catch (MyArraySizeException e) {
             e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
         }
+
+
     }
 
     //Метод по заданию для массива 4x4
-    public static void myArrayExchange(String[][] myArray) throws MyArraySizeException{
+    public static void myArrayExchange(String[][] myArray) throws MyArraySizeException,MyArrayDataException{
 
         if (myArray.length != 4 || myArray[0].length != 4){
             String exMsg = "Передан массив размерностью " + myArray.length + "x" + myArray[0].length;
@@ -21,11 +26,12 @@ public class Main {
         }else
         {
             System.out.println("Правильная размерность!");
+            throw new MyArrayDataException("!");
         }
     }
 
     //Метод по заданию для массива с переменной размерностью dimArr
-    public static void myArrayExchange(String[][] myArray, int dimArr) throws MyArraySizeException{
+    public static void myArrayExchange(String[][] myArray, int dimArr) throws MyArraySizeException,MyArrayDataException{
         if (myArray.length != dimArr || myArray[0].length != dimArr){
             String exMsg = "Передан массив размерностью " + myArray.length + "x" + myArray[0].length;
             exMsg += ". Массив должен быть размерностью " + dimArr + "x" + dimArr +".";
@@ -33,6 +39,7 @@ public class Main {
         }else
         {
             System.out.println("Правильная размерность!");
+            throw new MyArrayDataException("!");
         }
     }
 }
