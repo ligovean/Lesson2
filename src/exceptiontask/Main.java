0afa6b0add0ce26436703973f4b6dc50af38ca86
ligovean <ligomail@rambler.ru>
2019-05-07 21:25:01 +0300

    Enum and Exceptions v2.0

--- src/exceptiontask/Main.java
+++ src/exceptiontask/Main.java
@@ -2,79 +2,63 @@ package exceptiontask;
 
 import sun.invoke.empty.Empty;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //String[][] arr1 = {{"1","2","3"},{"01","02","03"},{"01","02","03"}};
-        String[][] arr1 = {{"1","2","3","4","5"},{"01","02","03","04","06"},{"01","02","03","04","001"},{"1","2","3","4","2"},{"1","2","3","4","07"}};
-
+        //String[][] arr1 = {{"1","2","3","4"},{"01","02!","03","04"},{"01","02","03","04"},{"01","02","03","04"}};
+        String[][] arr1 = {{"1","2","3","4","5"},{"01","03","03","04","06"},{"01=","02","03","04","001"},{"1","2","3","4","2"},{"1","2","3","4","07"}};
 
         try {
-            myArrayExchange(arr1,5);
+            System.out.println(myArrayEx(arr1,5));
         } catch (MyArraySizeException e) {
             e.printStackTrace();
         } catch (MyArrayDataException e) {
             e.printStackTrace();
         }
-
     }
 
-    //Метод по заданию для массива 4x4
-    public static void myArrayExchange(String[][] myArray) throws MyArraySizeException,MyArrayDataException{
-
-        if (myArray.length != 4 || myArray[0].length != 4){
-            String exMsg = "Передан массив размерностью " + myArray.length + "x" + myArray[0].length;
-            exMsg += ". Массив должен быть размерностью 4x4.";
-            throw new MyArraySizeException(exMsg);
-        }else{
-            String errCells ="";
-            char sep = ',';
-            int sumArray =0;
-
-            for (int i =0; i<(4*4);i++) {
-                int x = (int) i / 4;
-                int y = i % 4;
-                try {
-                    sumArray += Integer.parseInt(myArray[x][y]);
-                } catch (NumberFormatException e){
-                    if (errCells != "")
-                        errCells += sep + "[" + x + "," + y +"]";
-                    else
-                        errCells += "[" + x + "," + y +"]";
-                }
-            }
-            if (errCells != "")
-                throw new MyArrayDataException("В ячейках массива: " + errCells + " не числа!");
-            System.out.println("Сумма всех ячеек массива ровна " + sumArray + ".");
-        }
+    //Метод суммирования эелементов массива
+    private static int myArrayEx(String[][] arr, int dimArr) throws MyArrayDataException,MyArraySizeException {
+        checkSize(arr,dimArr);
+        checkData(arr,dimArr);
+       try{
+           int sumArray=0;
+           for (int i =0; i<(dimArr*dimArr);i++) {
+               sumArray += Integer.parseInt(arr[i/dimArr][i%dimArr]);
+           }
+        return sumArray;
+        }catch(NumberFormatException e){
+           return 0;
+       }
     }
 
-    //Перегрузка метода для массива с переменной размерностью dimArr
-    public static void myArrayExchange(String[][] myArray, int dimArr) throws MyArraySizeException,MyArrayDataException{
-        if (myArray.length != dimArr || myArray[0].length != dimArr){
-            String exMsg = "Передан массив размерностью " + myArray.length + "x" + myArray[0].length;
+    //Метод проверки размерности массива
+    private static void checkSize(String [][] arr, int dimArr) throws MyArraySizeException{
+        if (arr.length != dimArr || arr[0].length != dimArr){
+            String exMsg = "Передан массив размерностью " + arr.length + "x" + arr[0].length;
             exMsg += ". Массив должен быть размерностью " + dimArr + "x" + dimArr +".";
             throw new MyArraySizeException(exMsg);
-        }else
-        {
-            String errCells ="";
-            char sep = ',';
-            int sumArray =0;
-
-            for (int i =0; i<(dimArr*dimArr);i++) {
-                int x = (int) i / dimArr;
-                int y = i % dimArr;
-                try {
-                    sumArray += Integer.parseInt(myArray[x][y]);
-                } catch (NumberFormatException e){
-                    if (errCells != "")
-                        errCells += sep + "[" + x + "," + y +"]";
-                    else
-                        errCells += "[" + x + "," + y +"]";
-                }
-            }
-            if (errCells != "")
-                throw new MyArrayDataException("В ячейках: " + errCells + " не числа!");
-            System.out.println("Сумма всех ячеек массива ровна " + sumArray + ".");
         }
+    }
+
+    //Метод проверки данных массива
+    private static void checkData(String [][] arr, int dimArr)throws MyArrayDataException{
+        String errCells="";
+        for (int i =0; i<(dimArr*dimArr);i++) {
+            int x = i/dimArr;
+            int y = i%dimArr;
+            char sep = ',';
+            try {
+                Integer.parseInt(arr[x][y]);
+            }catch (NumberFormatException e){
+                if (errCells != "")
+                    errCells += sep + "[" + x + "," + y +"]";
+                else
+                    errCells += "[" + x + "," + y +"]";
+            }
+        }
+        if (errCells != "")
+            throw new MyArrayDataException("В ячейках: " + errCells + " не числа!");
     }
 }
